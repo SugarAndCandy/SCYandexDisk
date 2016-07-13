028d29d702626bd41c90275f79bd58a2715ba993
Maxim Kolesnik <maxim@sugarandcandy.ru>
2016-07-13 16:21:25 +0300

    Initial Commit

---
+++ SCYandexDisk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCYandexDisk
+//
+//  Created by Maxim Kolesnik on 13.07.16.
+//  Copyright © 2016 Sugar and Candy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SCYandexDisk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SCYandexDisk
+//
+//  Created by Maxim Kolesnik on 13.07.16.
+//  Copyright © 2016 Sugar and Candy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
